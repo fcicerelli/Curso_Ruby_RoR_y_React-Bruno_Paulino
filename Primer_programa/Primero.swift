2541afff8b4bb6389ce623fedca4eb03b40783e5
fcicerelli <fcicerellitic@gmail.com>
2025-12-06 11:36:23 -0300

    variables

--- Primer_programa/Primero.swift
+++ Primer_programa/Primero.swift
@@ -19,3 +19,27 @@ let implicitDouble = 70.0
 print(implicitDouble)
 let explicitDouble: Double = 70
 print(explicitDouble)
+
+print(" ")
+
+let constante: Float = 4
+print(constante)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+// let widthLabel = label + width
+// da error xq no se mezclan String e Int
+print(widthLabel)
+
+print(" ")
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+print(appleSummary)
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+print(fruitSummary)
+
+let otherDouble = 12.5
+let greeting = "Hello, \(otherDouble + implicitDouble)"
+print(greeting)
