402768e7d6370f120fab6151ef99517f46827245
fcicerelli <fcicerellitic@gmail.com>
2025-12-06 11:20:35 -0300

    first commit

---
+++ Primer_programa/Primero.swift
@@ -0,0 +1,21 @@
+// Hola Mundo
+print("Hello, world!")
+
+// Declaración de variables
+var myVariable = 42
+print(myVariable)
+
+// Asignación de variables
+myVariable = 50
+print(myVariable)
+
+// Constantes
+let myConstant = 42
+print(myConstant)
+
+let implicitInteger = 70
+print(implicitInteger)
+let implicitDouble = 70.0
+print(implicitDouble)
+let explicitDouble: Double = 70
+print(explicitDouble)
